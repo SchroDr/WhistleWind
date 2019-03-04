bac5fb17533467762929d77c70f065990a816457
SchroDr <1670057209@qq.com>
2019-03-04 22:00:47 +0800

    修改urls.py

--- WhistleWind/urls.py
+++ WhistleWind/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url(r'ww/', include(('WW.urls', 'WW'), namespace = 'WW'))
 ]
