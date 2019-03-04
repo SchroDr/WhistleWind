b3ae8ec9243fe44fee1ff821d33a6f5822380b98
SchroDr <1670057209@qq.com>
2019-03-04 22:09:27 +0800

    Update urls.py

--- WW/urls.py
+++ WW/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^login/$', views.login, name = 'login'),
     url(r'^register/$', views.register, name = 'register'),
+    url(r'^getMessages/$', views.getMessages, name = 'getMessages'),
+    url(r'^getMsgInfo/$', views.getMsgInfo, name = 'getMsgInfo'),
      
 ]
