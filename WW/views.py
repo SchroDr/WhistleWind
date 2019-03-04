ac87fcbdb7de4595028e4b190bc7ea60baa05eab
SchroDr <1670057209@qq.com>
2019-03-04 22:07:50 +0800

    Update views.py

--- WW/views.py
+++ WW/views.py
@@ -9,16 +9,16 @@ def login(request):
         'isWrong': 0,
         'userId': ''
     }
-    username = request.POST.get('username')
+    email = request.POST.get('email')
     password = request.POST.get('password')
-    user = models.User.objects.filter(user_name = username)
+    user = models.User.objects.filter(email = email)
     if len(user) == 0:
         result['isNotExit'] = 1
-        result['userId'] = username
+        result['userId'] = ""
         return JsonResponse(result)
     if user[0].password == password:
         result['isSucceed'] = 1
-        result['userId'] = username
+        result['userId'] = user[0].unique_ID
         return JsonResponse(result)
     result['isWrong'] = 1
     return JsonResponse(result)
@@ -28,17 +28,58 @@ def register(request):
         'isSucceed': 0,
         'isExist': 0
     }
-    userId = request.POST.get('userId')
+    email = request.POST.get('email')
     username = request.POST.get('username')
     password = request.POST.get('password')
-    user = models.User.objects.filter(user_name = username)
+    user = models.User.objects.filter(email = email)
     if len(user) == 1:
         result['isExit'] = 1
         return JsonResponse(result)
-    user = models.User.objects.create(user_Id)
+    user = models.User.objects.create(email = email, user_name = username, password = password)
+    result['isSucceed'] = 1
+    return JsonResponse(result)
 
 
-def post(request):
-    pass
-    pass
+def getMessages(request):
+    one_result = {
+        'title': '',
+        'content': '',
+        'img': {},
+        'msgId': '',
+        'x': '',
+        'y': ''
+    }
+    result = []
+    x = request.POST.get('x')
+    y = request.POST.get('y')
+    zoom = request.POST.get('zoom')
+    width = request.POST.get('width')
+    height = request.POST.get('height')
+    messages = models.Message.objects.filter(pos_x__gte = x-width, pos_x__lte = x+width, pos_y__gte = y-width, pos_y__lte = y+width)    
+    for message in messages:
+        one_result['title'] = message.title
+        one_result['content'] = message.content 
+        one_result['img'] = message.img 
+        one_result['msgId'] = message.msg_ID
+        one_result['x'] = message.pos_x
+        one_result['y'] = message.pos_y
+        result.append(one_result)
+    return JsonResponse(result, safe = False)
 
+def getMsgInfo(request):
+    message = models.Message.objectes.get(msg_ID = request.POST.get('msgID'))
+    author = models.User.objects.get(unique_ID = message.author)
+    result = {
+        'name': author.user_name,
+        'userID': author.unique_ID,
+        'headerImgUrl': author.avatar_name,
+        'like': message.like,
+        'dislike': message.dislike,
+        'time': message.add_date,
+        'imgUrl': message.img,
+        'comments': message.comments        
+    }
+
+def giveALike(request):
+    message = models.Message.objects.get(msg_ID = request.POST.get('msgId'))
+    user = models.User.objects.get(unique_ID = request.POST.get('userId'))
