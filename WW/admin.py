cdd47b173f2c2d1b0ada85d45898c8ae3f426d7e
SchroDr <1670057209@qq.com>
2019-07-13 00:38:45 +0800

    再次更新

--- WW/admin.py
+++ WW/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from .models import User, Message, Comment
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('unique_ID', 'email', 'user_name', 'avatar_name')
+    list_display = ('id', 'email', 'username', 'avatar')
 
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('msg_ID', 'pos_x', 'pos_y', 'title', 'author')
+    list_display = ('id', 'pos_x', 'pos_y', 'title', 'author')
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('comment_ID', 'msg_ID', 'user_ID')
+    list_display = ('id', 'msg', 'author')
 
 
 admin.site.register(User, UserAdmin)
--- WW/models.py
+++ WW/models.py
@@ -1,23 +1,6 @@
 from django.db import models
 import django.utils.timezone as timezone
 
-"""
-class User(models.Model):
-    unique_ID = models.AutoField("用户唯一标识符", primary_key = True)
-    email = models.CharField("email", max_length = 32, null = False, default = "")
-    user_name = models.CharField("用户名", max_length = 32, null = False, default = "user")
-    password = models.CharField("密码", max_length = 32, null = False, default = "000000")
-    avatar_name = models.ImageField("头像存储名", upload_to = "avatars", null = False, default = "rua.jpg")
-    follows = models.TextField("关注用户", default = '[]')
-    fans = models.TextField("粉丝", default = '[]')
-    msgs = models.TextField("所发推文", default = '[]')
-    comments = models.TextField("所发评论", default = '[]')
-    friends = models.TextField("好友", default = '[]')
-    introductino = models.TextField("简介", default = 'Hello, World')
-
-    def __str__(self):
-        return str(self.unique_ID)
-"""
 
 class User(models.Model):
     id = models.AutoField("用户唯一标识符", primary_key = True)
@@ -25,9 +8,8 @@ class User(models.Model):
     username = models.CharField("用户名", max_length = 62, null = False, default = "WWer")
     password = models.CharField("密码", max_length = 32, null = False)
     avatar = models.ImageField("头像", upload_to = "avatars", null = False, default = "rua.jpg")
-    follows = models.ManyToManyField("关注用户", User)
-    fans = models.ManyToManyField("粉丝", User)
-    friends = models.ManyToManyField("好友", User)
+    follows = models.ManyToManyField("self", verbose_name = "关注用户", through = "Followship", symmetrical=False, related_name='follow_set')
+    friends = models.ManyToManyField("self", verbose_name = "好友", through = "Friendship", symmetrical=False, related_name='friend_set')
     introduction = models.TextField("简介", default = "Hello，World")
 
     class Meta:
@@ -41,44 +23,24 @@ class User(models.Model):
         #return reverse("User_detail", kwargs={"pk": self.pk})
         pass
 
-"""
-class Message(models.Model):
-    msg_ID = models.AutoField("信息唯一标识符", primary_key = True)
-    pos_x = models.FloatField(default = 0)
-    pos_y = models.FloatField(default = 0)
-    title = models.CharField("标题", max_length = 64, null = False, default = "Title")
-    content = models.TextField("内容", default = "Content")
-    img = models.TextField("照片名", default = "Pic")
-    author = models.IntegerField("作者", default = 0)
-    like = models.IntegerField("点赞数", default = 0)
-    dislike = models.IntegerField("点踩数", default = 0)
-    who_like = models.TextField("点赞用户", default = '[]')
-    who_dislike = models.TextField("点踩用户", default = '[]')
-    add_date = models.DateTimeField('保存日期', default = timezone.now)
-    mod_date = models.DateTimeField('最后修改日期', auto_now = True)
-    comments = models.TextField("评论", default = '[]')
-
-    def __str__(self):
-        return str(self.msg_ID)
-"""
 
 class Message(models.Model):
     id = models.AutoField("信息唯一标识符", primary_key = True)
     pos_x = models.FloatField("信息位置经度", default = 0)
-    pos_y = models.FloatField("信息位置纬度", default= = 0)
+    pos_y = models.FloatField("信息位置纬度", default = 0)
     title = models.CharField("信息标题", max_length = 62)
     content = models.TextField("信息内容")
-    author = models.ManyToManyField(User, verbose_name = "该信息的作者")
+    author = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name = "该信息作者", related_name='message_author_set')
     like = models.IntegerField("点赞数", default = 0)
-    dislke = models.IntegerField("点踩数", default = 0)
-    who_like = models.ManyToManyField(User, verbose_name = "点赞该信息的用户")
-    who_dislike = models.ManyToManyField(User, verbose_name = "点踩该信息的用户")
+    dislike = models.IntegerField("点踩数", default = 0)
+    who_like = models.ManyToManyField(User, verbose_name = "点赞该信息的用户", related_name='message_who_like_set')
+    who_dislike = models.ManyToManyField(User, verbose_name = "点踩该信息的用户", related_name='message_who_dislike_set')
     add_date = models.DateField("发布日期", default = timezone.now)
     mod_date = models.DateField("最后修改日期", auto_now = True)
 
     class Meta:
-        verbose_name = _("Message")
-        verbose_name_plural = _("Messages")
+        verbose_name = ("Message")
+        verbose_name_plural = ("Messages")
 
     def __str__(self):
         return self.id
@@ -87,36 +49,21 @@ class Message(models.Model):
         #return reverse("Message_detail", kwargs={"pk": self.pk})
         pass
 
-"""
-class Comment(models.Model):
-    comment_ID = models.AutoField("评论唯一标识符", primary_key = True)
-    msg_ID = models.IntegerField("所评论信息唯一标识符", default = 0)
-    user_ID = models.IntegerField("评论用户唯一标识符", default = 0)
-    content = models.TextField("评论内容", default = 'Content')
-    img = models.ImageField("评论图片", upload_to = "command_img", null = True)
-    like = models.IntegerField("点赞数", default = 0)
-    who_like = models.TextField("点赞用户", default = '[]')
-    add_date = models.DateTimeField('保存日期',default = timezone.now)
-    mod_date = models.DateTimeField('最后修改日期', auto_now = True)
-
-    def __str__(self):
-        return str(self.comment_ID)
-"""
 
 class Comment(models.Model):
     id = models.AutoField("评论唯一标识符", primary_key = True)
-    msg = models.ForeignKey(Message, on_delete = models.CASCADE, verbose_name = "该评论所属信息")
-    author = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name = "该评论所属用户")
+    msg = models.ForeignKey(Message, on_delete = models.CASCADE, verbose_name = "该评论所属信息", default = '')
+    author = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name = "该评论所属用户", related_name='comment_author_set', default = '')
     content = models.TextField("评论内容", default = "Hello")
     like = models.IntegerField("点赞数", default = 0)
-    who_like = models.ManyToManyField(User, verbose_name = "点赞该评论的用户")
+    who_like = models.ManyToManyField(User, verbose_name = "点赞该评论的用户", related_name='comment_who_like_set')
     add_date = models.DateTimeField('保存日期',default = timezone.now)
     mod_date = models.DateTimeField('最后修改日期', auto_now = True)
     
 
     class Meta:
-        verbose_name = _("Comment")
-        verbose_name_plural = _("Comments")
+        verbose_name = ("Comment")
+        verbose_name_plural = ("Comments")
 
     def __str__(self):
         return self.name
@@ -157,3 +104,14 @@ class CommentImage(models.Model):
     def get_absolute_url(self):
         #return reverse("CommentImage_detail", kwargs={"pk": self.pk})
         pass
+
+
+class Friendship(models.Model):
+    initiator = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name = "发起申请的用户", related_name='initiator_set')
+    recipient = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name = "接受申请的用户", related_name='recipient_set')
+    date = models.DateField("成为好友的日期", default = timezone.now)
+
+class Followship(models.Model):
+    followed_user = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name = "被关注者", related_name='followed_user_set')
+    fan = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name = "粉丝", related_name='fan_set')
+    date = models.DateField("开始关注的日期", default = timezone.now)
